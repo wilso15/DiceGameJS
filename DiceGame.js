94e7ff107a1303f9e3cee52cf61bb868710ded4b
danielyang1 <dan0yang@outlook.com>
2016-01-05 16:51:05 -0600

    deleted semicolons and added code

--- DiceGame.js
+++ DiceGame.js
@@ -1,26 +1,40 @@
 "use strict";
 
-function nameEntry();{
+function nameEntry() {
 
 }
 
 
-function totalScore();{
+function totalScore() {
 
 }
 
-/*----------------------- Dan ----------------------*/
-function chooseWinningNumber();{
+/*vvvvvvvvvvvvvvvvvvv Dan vvvvvvvvvvvvvv-Choose a number to play to-vvvvvvvvvvvvvvvv*/
+function winningNumber() {
+    var playTo;
+    playTo = prompt("Play to 13, 21 or 35!");
+    if (playTo == 13){
+        alert("First to 13 wins! Good luck!");
+        return playTo;
+    } else if (playTo == 21) {
+        alert("First to 21 wins! Good luck!");
+        return playTo;
+    } else if (playTo == 35) {
+        alert("First to 35 wins! Good luck!");
+        return playTo;
+    } else {
+        alert("FOLLOW DIRECTIONS!!");
+        winningNumber();
+    }
+};
+/*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Dan ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
 
-}
-/*----------------------- Dan ----------------------*/
 /*----------------------- Nick ----------------------*/
-function decideFirstRoller();{
+function decideFirstRoller() {
 
 }
 /*----------------------- Nick ----------------------*/
 
-function getRandomDiceRoll();{
+function getRandomDiceRoll() {
 
 }
-
