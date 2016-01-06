65bf39cc5883b4bb1169e29287a61fb1f99dfbc6
Nick Wilson <Nick Wilson>
2016-01-06 14:03:59 -0600

    cleaned up functions/naming

--- DiceGame.js
+++ DiceGame.js
@@ -1,27 +1,40 @@
 "use strict";
 
 var human1;
-human1 = myPerson1();
 var human2;
+human1 = myPerson1();
 human2 = myPerson2();
+human1score = 
+human2score = 
+playerdiceroll = getRandomDiceRoll
 
-function myPerson1(){
+
+function createPerson1(){
 	var person1; 
 	person1 = prompt("DiceGame: Player 1");
 	return person1;
-	
 }
-function myPerson2(){
+
+function createPerson2(){
 	var person2;
 	person2 = prompt("DiceGame: Player 2");
 	return person2;
 }
 
-function totalScore() {
-
+function getRandomDiceRoll() {
+var randDice 
+randDice= 1 + Math.floor(Math.random()*6);
+return(randDice); 
 }
 
-/*vvvvvvvvvvvvvvvvvvv Dan vvvvvvvvvvvvvv-Choose a number to play to-vvvvvvvvvvvvvvvv*/
+function AddScorePlayer(randomrollplayer) {
+    var scoreTotal 
+	scoreTotal = 0;
+    human1 = scoreTotal;
+    scoreTotal += randomrollplayer;
+}
+
+
 function winningNumber() {
     var playTo;
     playTo = prompt("Play to 13, 21 or 35!");
@@ -38,35 +51,13 @@ function winningNumber() {
         alert("FOLLOW DIRECTIONS!!");
         winningNumber();
     }
-};
-/*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Dan ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
+	}
 
-/*----------------------- Nick ----------------------*/
 
-function decideFirstRoller() {
-test
-test
-test
-test
-
-}
-/*----------------------- Nick ----------------------*/
-
-function getRandomDiceRoll() {
-var randDice 
-randDice= 1 + Math.floor(Math.random()*6);
-return(randDice); 
+game(){
+while ()
 }
 
-
-
-}
-
-function AddScorePlayer1(randomrollplayer1) {
-    var scoreTotal1 = 0;
-    myPerson1() = scoreTotal1;
-    scoreTotal1 += randomrollplayer1;
-}
 
 randomrollplayer1 = diceroll()
 
@@ -76,3 +67,5 @@ randomrollplayer1 = diceroll()
 */
 
 AddScorePlayer1();
+
+
