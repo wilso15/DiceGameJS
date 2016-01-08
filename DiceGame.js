eaa565a9ad58ecc91ebcafaf74ef143d74374aa0
danielyang1 <dan0yang@outlook.com>
2016-01-07 22:43:31 -0600

    minor updates for looks

--- DiceGame.js
+++ DiceGame.js
@@ -66,9 +66,9 @@ function runGame(playTo, person1, person2, p1diceRoll, p2diceRoll){
         if (p1totalScore > playTo && p2totalScore > playTo) {
             console.log(" * * BOTH Went over Limit of "+playTo+" * * ");
         } else if (p1totalScore > playTo) {
-            console.log(person1+" went over Limit of "+playTo+" * * ");
+            console.log(" * * "+person1+" went over Limit of "+playTo+" * * ");
         } else {
-            console.log(person2+" went over Limit of "+playTo+" * * ");
+            console.log(" * * "+person2+" went over Limit of "+playTo+" * * ");
         }
         alert("*** The Limit of "+playTo+" exceeded. Both Players Start Over!***");
         runGame(playTo, person1, person2, p1diceRoll, p2diceRoll);
